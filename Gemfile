2ee85af9f26c702f1efdea07280e6026827ebaae
krzykamil <krzysiekkamilpiotrek@gmail.com>
2022-03-04 16:42:59 +0100

    basis for ok looking blog posts with bulma

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'webpacker', '~> 5.0'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem 'faker'
-
+gem "bulma-rails", "~> 0.9.3"
 
 gem 'bootsnap', '>= 1.4.4', require: false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
   resources :posts
+  root to: "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
